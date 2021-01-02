b8ece0ea2afeed0965b5b96462394ef9eb806131
vivi <ashin-jo@yahoo.com.tw>
2021-01-02 13:13:14 +0800

    20210102

--- 解鎖畫面練習＿20201227/ViewController.swift
+++ 解鎖畫面練習＿20201227/ViewController.swift
@@ -8,19 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+//    定義四張密碼圖
     @IBOutlet weak var passcodeImage1: UIImageView!
     @IBOutlet weak var passcodeImage2: UIImageView!
     @IBOutlet weak var passcodeImage3: UIImageView!
     @IBOutlet weak var passcodeImage4: UIImageView!
-    
+//    定義label
     @IBOutlet weak var typeAndResultLabel: UILabel!
+//    定義密碼
     var corectpasscode = "0528"
     var typepasscode = ""
-//    let typeImageName = ["favorite1", "favorite1", "favorite1", "favorite1"]
-//
-    @IBOutlet weak var typePasscodeImage: UIImageView!
     
+//設定密碼輸入的圖片動作
     func passwordImage() {
         let passcodeCount = typepasscode.count
         
@@ -57,6 +56,7 @@ class ViewController: UIViewController {
         }
         
     }
+//    設定輸入密碼的結果反應
     func keyInPassword(keyIn: String){
         if 0...3 ~= typepasscode.count{
             typepasscode += keyIn
@@ -71,6 +71,7 @@ class ViewController: UIViewController {
         }
        passwordImage()
     }
+//    設定重新開始動作
     func restart(){
         typepasscode = ""
         clearNumber()
@@ -78,6 +79,7 @@ class ViewController: UIViewController {
         typeAndResultLabel.text = "Enter your promise. "
 
     }
+//    設定清除密碼動作
     func clearNumber(){
         passcodeImage1.image = UIImage(named: "favorite1")
         passcodeImage2.image = UIImage(named: "favorite1")
@@ -85,18 +87,18 @@ class ViewController: UIViewController {
         passcodeImage4.image = UIImage(named: "favorite1")
         typeAndResultLabel.text = "Enter your promise. "
        }
-
+//輸入密碼function
     @IBAction func numberBtn(_ sender: UIButton) {
         keyInPassword(keyIn: String(sender.tag))
     }
-    
+//刪除密碼function
     @IBAction func deleteBtn(_ sender: UIButton) {
         if 1...3 ~= typepasscode.count {
             typepasscode.removeLast()
         }
         passwordImage()
     }
-
+//重新輸入function
     @IBAction func reloadBtn(_ sender: UIButton) {
         restart()
     }
